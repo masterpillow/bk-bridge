380ba46de695ebb543d34802a23c521b902fc191
Imad Antonios <8104351+iantonios@users.noreply.github.com>
2023-11-11 21:50:46 -0500

    modified subheader

--- streamlit-temporal-local.py
+++ streamlit-temporal-local.py
@@ -10,9 +10,9 @@ bridge = pd.read_csv(filename,
                      parse_dates=True,
                      index_col='hour_beginning')
 
-st.subheader('Brooklyn Bridge Pedestrian Crossings')
+st.subheader('Brooklyn Bridge Pedestrian Crossings - Imad Antonios')
 st.markdown('---')
-volume = st.radio("Pedestrian Traffic Volume",('HR', 'DAY', 'WEEK'))
+volume = st.radio("Pedestrian Traffic Volume",('Hourly', 'Daily', 'Weekly'))
 st.markdown('---')
 
 if volume == 'Hourly':
