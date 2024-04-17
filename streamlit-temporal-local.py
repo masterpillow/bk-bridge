b4e4772606bd4e1c02b4790a5f1f486693793ba4
Max Musial <144265654+masterpillow@users.noreply.github.com>
2024-04-17 15:46:27 -0400

    Update streamlit-temporal-local.py

--- streamlit-temporal-local.py
+++ streamlit-temporal-local.py
@@ -10,7 +10,7 @@ bridge = pd.read_csv(filename,
                      parse_dates=True,
                      index_col='hour_beginning')
 
-st.subheader('Brooklyn Bridge Pedestrian Crossings - Imad Antonios')
+st.subheader('Brooklyn Bridge Pedestrian Crossings - Max Musial')
 st.markdown('---')
 volume = st.radio("Pedestrian Traffic Volume",('Hourly', 'Daily', 'Weekly'))
 st.markdown('---')
